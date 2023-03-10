17eec5848726cdfcabcea40141453d4097aeebb7
Stanislav2022 <tsv5@ukr.net>
2023-03-10 13:10:57 +0200

    2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Helmet } from 'react-helmet';
+import { LoginForm } from 'components/LoginForm/LoginForm';
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <LoginForm />
+    </div>
+  );
 }
