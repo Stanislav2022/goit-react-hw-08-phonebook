fe62ad24f25b7cf5051b1bcf3bdd50597ce897fd
Stanislav2022 <tsv5@ukr.net>
2023-03-10 14:49:47 +0200

    2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { useDispatch } from 'react-redux';
+import { addContact} from 'redux/contacts/contacts-operation';
 
-export default function ContactForm() {
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      const { name, phone } = e.target.elements;
+    const form = e.target;
+    dispatch(addContact({name: name.value, phone: phone.value}));
+    form.reset();
+  };
+
   return (
-    <div>ContactForm</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <lable>
+        <input type="text" name="name" pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan" />
+      </lable>
+      <lable>
+        <input
+            type="tel"
+            name="phone"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        />
+      </lable>
+      <button type="submit">
+        Add task
+      </button>
+   </form>
+
+  );
+};
+
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { ContactList } from 'components/ContactList/ContactList';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { fetchContacts } from 'redux/contacts/contacts-operation';
+import { getLoading } from 'redux/contacts/contacts-selector';
 
 export default function Contacts() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <h2>Contacts</h2>
-  )
+    <>
+      <Helmet>
+        <title>Your ContactList</title>
+      </Helmet>
+      <ContactForm />
+      <div>{isLoading && 'Request in progress...'}</div>
+      <ContactList />
+    </>
+  );
 }
