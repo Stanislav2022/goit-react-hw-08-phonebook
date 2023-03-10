6ef151d2e3521cf739dbcd9a74756514528252d6
Stanislav2022 <tsv5@ukr.net>
2023-03-10 12:30:06 +0200

    2

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -22,15 +22,15 @@ export const RegisterForm = () => {
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
       <label className={css.label}>
         Username
-        <input type="text" name="text" />
+        <input type="text" name="text"/>
       </label>
       <label className={css.label}>
         Email
-        <input type="email" name="email" />
+        <input type="email" name="email"/>
       </label>
       <label className={css.label}>
         Password
-        <input type="password" name="password" />
+        <input type="password" name="password"/>
       </label>
       <button type="submit">Register</button>
     </form>
